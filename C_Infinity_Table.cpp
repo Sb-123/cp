59296c139595e224431a2f8d9ace62917e7a0a05
sanjeev <sb8031151@gmail.com>
2023-05-20 15:48:15 +0530

    Latest question

---
+++ C_Infinity_Table.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol()
+{
+    int k;
+    cin >> k;
+}
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+}
--- codeforces/pretty_permutatiion.cpp
+++ codeforces/pretty_permutatiion.cpp
@@ -1,53 +1,71 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-void sol(){
-    int n;cin>>n;
+void sol()
+{
+    int n;
+    cin >> n;
     int a[n];
-   
 
-    if(n%2==0){
-        int ev=2;
-        int od=1;
-        for(int i=0;i<n;i++){
-            if(i%2){
-                cout<<od<<" ";
-                od +=2;
-            }else{
-                cout<<ev<<" ";
-                ev +=2;
+    if (n % 2 == 0)
+    {
+        int ev = 2;
+        int od = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2)
+            {
+                cout << od << " ";
+                od += 2;
+            }
+            else
+            {
+                cout << ev << " ";
+                ev += 2;
             }
         }
-        cout<<"\n";
-    }else{
-        if(n==3){
-            cout<<3<<" "<<1<<" "<<2<<" ";
-        }
-    if(n>3){
-      cout<<"3"<<" "<<"1"<<" "<<"2"<<" ";
-        int od=5;
-        int ev=4;
-        for(int i=3;i<n;i++){
-            if(i%2){
-                cout<<od<<" ";
-                od +=2;
-            }else{
-                cout<<ev<<" ";
-                ev +=2;
-            }
-        }
-      }
-      cout<<"\n";
+        cout << "\n";
     }
-
-    
-    
+    else
+    {
+        if (n == 3)
+        {
+            cout << 3 << " " << 1 << " " << 2 << " ";
+        }
+        if (n > 3)
+        {
+            cout << "3"
+                 << " "
+                 << "1"
+                 << " "
+                 << "2"
+                 << " ";
+            int od = 5;
+            int ev = 4;
+            for (int i = 3; i < n; i++)
+            {
+                if (i % 2)
+                {
+                    cout << od << " ";
+                    od += 2;
+                }
+                else
+                {
+                    cout << ev << " ";
+                    ev += 2;
+                }
+            }
+        }
+        cout << "\n";
+    }
 }
 
-int32_t main(){
-    int t;cin>>t;
-    while(t--){
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
         sol();
     }
 }
