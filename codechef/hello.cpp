d8d460d94f45f967061001a9af7ede294f993c59
sanjeev <sb8031151@gmail.com>
2023-11-30 23:06:39 +0530

    a;lfj

---
+++ codechef/hello.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello World" << endl;
+
+    cout << 5 + 4 << endl;
+
+    cout << "Sanjeev Suman"
+         << "\n";
+
+    int a = 5, b = 8;
+    cout << a * b << "\n";
+
+    string s = "sanjeev";
+    cout << s[2] << "\n";
+
+    return 0;
+}
